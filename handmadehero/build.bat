fa8995720384513d213d85f9dddd5a58171a8c94
Konrad3453 <konrad.baj98@gmail.com>
2025-08-13 08:11:53 +0200

    compiler warnings and such

--- handmadehero/build.bat
+++ handmadehero/build.bat
@@ -5,7 +5,7 @@ set start_time=%TIME%
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 if not exist ..\build mkdir ..\build
 pushd ..\build
-cl -WX -W4 -wd4201 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Zi ..\handmadehero\handmadehero.cpp  user32.lib gdi32.lib 
+cl -WX -W4 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Zi ..\handmadehero\handmadehero.cpp  user32.lib gdi32.lib 
 popd
 
 echo Build started at %start_time%
@@ -15,3 +15,5 @@ echo Build finished at %TIME%
 :: -WX warnings as errors
 :: -W4 warning lvl 4
 :: -wd[warning number] ignore warning
+::    4100 unused param
+::   
