4e0b6c14ab841e789ac4e39fed0161e8cc1600e6
Konrad3453 <konrad.baj98@gmail.com>
2025-09-16 23:06:11 +0200

    framecount

--- push.bat
+++ push.bat
@@ -14,4 +14,16 @@ git commit -m %1
 echo Pushing to GitHub...
 git push
 
-echo Done!
+
+REM Enable ANSI escape sequences (Windows 10 or later required)
+for /f "tokens=2 delims=[]" %%i in ('ver') do set version=%%i
+if not "%version%"=="Version 10.0" goto :done
+
+>nul 2>&1 reg query "HKCU\Console" /v VirtualTerminalLevel
+if errorlevel 1 (
+    reg add "HKCU\Console" /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul
+)
+
+:done
+REM Output green text using ANSI escape sequence
+echo [[32mDone![0m]
